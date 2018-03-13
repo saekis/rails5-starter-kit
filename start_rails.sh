7ccf4c6df95d521b59739a66c2e8c4b514546929
saekis <s.saeki71@gmail.com>
2018-03-13 17:57:40 +0900

    change database type to variable

--- start_rails.sh
+++ start_rails.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 RUBY_VERSION=2.5.0
+DATABASE=mysql
 
 ## Set ruby version
 rbenv local $RUBY_VERSION
@@ -9,4 +10,4 @@ ruby -v
 gem install bundler
 bundle install --path vendor/bundle --jobs=4
 
-bundle exec rails new . -f -d mysql --skip-turbolinks --skip-test
+bundle exec rails new . -f -d $DATABASE --skip-turbolinks --skip-test
