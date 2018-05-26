f2a72945cba2ea92549ec3487c80951dd57e3856
saekis <s.saeki71@gmail.com>
2018-05-26 22:17:05 +0900

    update start shell

--- start_rails.sh
+++ start_rails.sh
@@ -5,7 +5,7 @@ down() {
     echo
     echo "clean up containers..."
 
-    docker-compose down
+    docker-compose -f ./docker/docker-compose.yml down
 
     echo "done."
     echo
@@ -15,11 +15,10 @@ trap down HUP TERM INT;
 
 if [ ! -d "./app" ]; then
     docker-compose -f ./docker/docker-compose.yml run --rm rails rails new . -f -d mysql --skip-turbolinks --skip-test
-    cp -R ./docker/rails/init/.bundle ./.bundle
     docker-compose -f ./docker/docker-compose.yml run --rm rails bundle install
     cp ./docker/rails/init/puma.rb ./config/puma.rb
     cp ./docker/rails/init/database.yml ./config/database.yml
-    docker-compose run -f ./docker/docker-compose.yml --rm rails rake db:create
+    docker-compose -f ./docker/docker-compose.yml run --rm rails rake db:create
 fi
 
 docker-compose -f ./docker/docker-compose.yml run --rm rails bundle install
