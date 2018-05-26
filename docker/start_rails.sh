5389fdac4c381deb1cea1a50f15b33a27d288556
saekis <s.saeki71@gmail.com>
2018-05-26 22:31:00 +0900

    mv docker file to docker dir

---
+++ docker/start_rails.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+down() {
+    echo
+    echo "clean up containers..."
+
+    docker-compose -f ./docker-compose.yml down
+
+    echo "done."
+    echo
+}
+
+trap down HUP TERM INT;
+
+if [ ! -d "./app" ]; then
+    cp ./rails/init/.bundle ../.bundle
+    cp ./rails/init/Gemfile ../Gemfile
+    cp ./rails/init/Gemfile.lock ../Gemfile.lock
+    docker-compose -f ./docker-compose.yml run --rm rails rails new . -f -d mysql --skip-turbolinks --skip-test
+    docker-compose -f ./docker-compose.yml run --rm rails bundle install
+    cp ./rails/init/puma.rb ../config/puma.rb
+    cp ./rails/init/database.yml ../config/database.yml
+    docker-compose -f ./docker-compose.yml run --rm rails rake db:create
+fi
+
+docker-compose -f ./docker-compose.yml run --rm rails bundle install
+docker-compose -f ./docker-compose.yml up
